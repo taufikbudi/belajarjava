9fcd791f57fa4eaf1843a3e38d7ae3586c027a3a
taufikbudi <taufikgate@gmail.com>
2020-08-07 15:04:59 +0700

    update

--- src/main/java/com/employeleave/requestemployeleave/models/Position.java
+++ src/main/java/com/employeleave/requestemployeleave/models/Position.java
@@ -2,15 +2,12 @@ package com.employeleave.requestemployeleave.models;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-//ini models position
-@Entity
-@Table(name = "position",schema = "public")
+
+
 public class Position {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "position_id_seq")
+
     private Long id;
-    @Column
+
     private Long positionId;
     private String positionName;
     private String createdBy;
