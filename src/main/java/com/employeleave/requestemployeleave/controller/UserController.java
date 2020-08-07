7db8551c60bc124adf14cce9c8afd0967f44f0d6
jaka junaedi <62337311+JakaJunaedi@users.noreply.github.com>
2020-08-07 11:16:30 +0700

    commit byjack (#2)

--- src/main/java/com/employeleave/requestemployeleave/controller/UserController.java
+++ src/main/java/com/employeleave/requestemployeleave/controller/UserController.java
@@ -5,7 +5,7 @@ import com.employeleave.requestemployeleave.models.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.io.iona.springboot.controllers.HibernateCRUDController;
-
+// by jack //
 @RestController
 @RequestMapping("api/user")
 public class UserController extends HibernateCRUDController  <User, UserDTO> {
