f2febc92a0ad70da164964053996b8b67ddaf513
taufikbudi <taufikgate@gmail.com>
2020-08-11 10:23:30 +0700

    development
    penambahan position

--- src/main/java/com/employeleave/requestemployeleave/models/Position.java
+++ src/main/java/com/employeleave/requestemployeleave/models/Position.java
@@ -3,16 +3,80 @@ package com.employeleave.requestemployeleave.models;
 import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Entity
+@Table(name = "position",schema = "public")
 
 public class Position {
-
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "position_id_seq")
     private Long id;
-
+    @Column(name = "position_id")
     private Long positionId;
+    @Column(name = "position_name")
     private String positionName;
+    @Column(name = "created_by")
     private String createdBy;
+    @Column(name = "updated_by")
     private String updatedBy;
+    @Column(name = "created_date")
     private Timestamp createdDate;
+    @Column(name = "updated_date")
     private Timestamp updatedDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Long positionId) {
+        this.positionId = positionId;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Timestamp getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Timestamp createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Timestamp getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Timestamp updatedDate) {
+        this.updatedDate = updatedDate;
+    }
 }
