fbdf83be3700d15ed37144e382e50cf208c32671
taufikbudi <taufikgate@gmail.com>
2020-08-13 11:14:09 +0700

    README.md

--- src/main/java/com/employeleave/requestemployeleave/restservicecors/RestServiceCorsApplication.java
+++ src/main/java/com/employeleave/requestemployeleave/restservicecors/RestServiceCorsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+//add
 @SpringBootApplication
 public class RestServiceCorsApplication  {
     public static void main(String[] args) {
